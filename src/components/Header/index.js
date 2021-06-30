ec3e7cd061f0f0ed0b5013c3436c664cf750f25b
facorda <acorda.fred@gmail.com>
2021-06-30 18:49:53 -0400

    3.5.3 created menu icon transition.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,17 @@ import { useState } from "react";
 import Link from "next/link";
 import styles from "./header.module.scss";
 
+import { motion } from "framer-motion";
+
 const Header = ({ categoryList = [] }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className={styles["header"]}>
+    // <header className={styles["header"]}>
+    <motion.header
+      className={styles["header"]}
+      animate={open ? "open" : "closed"}
+    >
       <div className={styles["header__wrapper"]}>
         <div className={styles["header__itemLeft"]}>
           {/* MENU BUTTON WILL GO HERE*/}
@@ -15,50 +21,39 @@ const Header = ({ categoryList = [] }) => {
             className={styles["header__menuBtn"]}
             onClick={() => setOpen(!open)}
           >
-            <svg width="40" height="40" viewBox="0 0 23 23">
-              {open ? (
-                <g>
-                  <path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="white"
-                    strokeLinecap="round"
-                    d="M 3 16.5 L 17 2.5"
-                  />
-                  <path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="white"
-                    strokeLinecap="round"
-                    d="M 3 2.5 L 17 16.346"
-                  />
-                </g>
-              ) : (
-                <g>
-                  <path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="white"
-                    strokeLinecap="round"
-                    d="M 2 2.5 L 20 2.5"
-                  />
-
-                  <path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="white"
-                    strokeLinecap="round"
-                    d="M 2 9.423 L 20 9.423"
-                  />
-                  <path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="white"
-                    strokeLinecap="round"
-                    d="M 2 16.346 L 20 16.346"
-                  />
-                </g>
-              )}
+            <svg width="40" height="40" viewBox="0 0 20 20">
+              <motion.path
+                fill="transparent"
+                strokeWidth="3"
+                stroke="#ffffff"
+                strokeLinecap="round"
+                variants={{
+                  closed: { d: "M 2 2.5 L 18 2.5" },
+                  open: { d: "M 3 16.5 L 17 2.5" },
+                }}
+              />
+              <motion.path
+                fill="transparent"
+                strokeWidth="3"
+                stroke="#ffffff"
+                strokeLinecap="round"
+                d="M 2 9.423 L 18 9.423"
+                variants={{
+                  closed: { opacity: 1 },
+                  open: { opacity: 0 },
+                }}
+                transition={{ duration: 0.1 }}
+              />
+              <motion.path
+                fill="transparent"
+                strokeWidth="3"
+                stroke="#ffffff"
+                strokeLinecap="round"
+                variants={{
+                  closed: { d: "M 2 16.346 L 18 16.346" },
+                  open: { d: "M 3 2.5 L 17 16.346" },
+                }}
+              />
             </svg>
           </button>
         </div>
@@ -88,7 +83,7 @@ const Header = ({ categoryList = [] }) => {
           ))}
         </nav>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
