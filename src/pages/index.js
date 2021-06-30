62574a9deea454001972385f49b61c9451556687
facorda <acorda.fred@gmail.com>
2021-06-30 11:56:19 -0400

    3.2.3 updated footer styles.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head';
+import Head from "next/head";
+import Footer from "../components/Footer";
 
-import { getRecentPosts } from '../utils/posts';
-import { getCategoryList } from '../utils/categories';
+import { getRecentPosts } from "../utils/posts";
+import { getCategoryList } from "../utils/categories";
 
 const Home = ({ recentPosts, categoryList }) => {
   return (
@@ -13,6 +14,7 @@ const Home = ({ recentPosts, categoryList }) => {
         <pre>{JSON.stringify(recentPosts, null, 2)}</pre>
         <pre>{JSON.stringify(categoryList, null, 2)}</pre>
       </main>
+      <Footer />
     </div>
   );
 };
